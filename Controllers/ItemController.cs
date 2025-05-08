c01ea706815ea0fab0c71967b803af9f0ce21883
Mátravölgyi Bendegúz <91052226+MatraBendi@users.noreply.github.com>
2025-05-08 12:45:33 +0000

    Add files via upload

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,11 @@
+﻿using DotNetNuke.Web.Mvc.Framework.Controllers;
+using System.Web.Mvc;
+
+public class ItemController : DnnController
+{
+    [HttpGet]
+    public ActionResult Index()
+    {
+        return View();  // az általad készített egyetlen Index.cshtml-t rendereli
+    }
+}
